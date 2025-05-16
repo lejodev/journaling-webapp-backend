86fa4db259e0514c602e951bfeaffc3d8c1c6193
Alejandro <alejandro.rv97@gmail.com>
2025-05-15 21:08:05 -0500

    Manual backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { JournalModule } from './modules/journal/journal.module';
+import { WrapperService } from './core/services/wrapper/wrapper.service';
 
 @Module({
   imports: [
     UserModule,
     JournalModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, WrapperService],
 })
 export class AppModule { }
---
+++ src/core/services/wrapper/wrapper.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { from, Observable } from 'rxjs';
+import { EntityManager, EntityTarget, FindOptionsOrder, FindOptionsWhere, UpdateResult } from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+
+@Injectable()
+export class WrapperService {
+
+    constructor(@InjectEntityManager() private entityManager: EntityManager) { }
+
+    GET<T>(
+        entity: EntityTarget<T>,
+        options?: {
+
+            where?: FindOptionsWhere<T> | FindOptionsWhere<T>[];
+            order?: FindOptionsOrder<T>;
+            relations?: string[];
+            take?: number;
+            skip?: number;
+        }
+
+    ): Observable<T[]> {
+        const findOptions = {
+            ...options
+        }
+        return from(this.entityManager.find(entity, findOptions))
+    }
+
+
+    findAll<T>(entityClass: EntityTarget<T>): Observable<T[]> {
+        return from(this.entityManager.find(entityClass))
+    }
+
+    findOne<T>(entityTarget: EntityTarget<T>, where: FindOptionsWhere<T>): Observable<T | null> {
+        return from(this.entityManager.findOneBy(entityTarget, where))
+    }
+
+    query<T>(query: string, parameters: string[],): Observable<T | null> {
+        return from(this.entityManager.query(query, parameters))
+    }
+
+    create<T>(entityTarget: EntityTarget<T>, body: T): Observable<T> {
+        return from(this.entityManager.save(entityTarget, body))
+    }
+
+    update<T>(entityTarget: EntityTarget<T>, criteria: any, data: QueryDeepPartialEntity<T>): Observable<UpdateResult> {
+        return from(this.entityManager.update(entityTarget, criteria, data))
+    }
+
+}
